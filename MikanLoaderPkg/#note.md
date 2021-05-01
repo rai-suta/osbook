9064bac0c1fef0df7951934e38a6ecdea5e25059
rai-suta <sn0425.flv+github@gmail.com>
2021-05-01 22:45:06 +0900

    2.5 メモリマップの取得 (osbook_day02b)

--- MikanLoaderPkg/#note.md
+++ MikanLoaderPkg/#note.md
@@ -28,3 +28,65 @@ $ build
 ```
 $ ~/osbook/devenv/run_qemu.sh ~/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi
 ```
+
+## 2.5 メモリマップの取得 (osbook_day02b)
+
+UEFI からメモリマップを取得して、ファイルに保存するプログラムを作成する
+
+[2] UEFI Specification Version 2.8 (Errata B) (released June 2020)
+
+> EFI_BOOT_SERVICES.GetMemoryMap() : Returns the current memory map.
+
+> EFI_BOOT_SERVICES.OpenProtocol() : Queries a handle to determine if it supports a specified protocol. 
+If the protocol is supported by the handle, it opens the protocol on behalf of the calling agent. 
+This is an extended version of the EFI boot service EFI_BOOT_SERVICES.HandleProtocol(). 
+
+> EFI_FILE_PROTOCOL.Open() : Opens a new file relative to the source file’s location.
+
+Main.c へ追加
+
+- MemoryMap : 'EFI_BOOT_SERVICES.GetMemoryMap()' から取得する情報を格納するデータ構造
+- GetMemoryMap() : UEFI からメモリマップを取得
+- GetMemoryTypeUnicode() : EFI_MEMORY_TYPE 型から要約文を取得
+- SaveMemoryMap() : メモリマップをCSVファイルに変換して保存する
+- OpenRootDir() : UEFI からルートディレクトリへのハンドルを取得
+
+Loader.inf へ追加
+
+```
+[Protocols]
+  gEfiLoadedImageProtocolGuid
+  gEfiLoadFileProtocolGuid
+  gEfiSimpleFileSystemProtocolGuid
+```
+
+## 2.7 メモリマップの確認
+
+Main.c をビルド
+```
+$ cd ~/edk2
+$ source edksetup.sh
+$ build
+```
+
+QEMU で Loader.efi を起動
+```
+$ ~/osbook/devenv/run_qemu.sh  Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi
+```
+
+QEMU を起動するとファイルシステムイメージが disk.img として作成されるのでこれをマウントして中身を確認できる。
+```
+$ mkdir -p mnt
+$ sudo mount -o loop disk.img mnt
+$ ls mnt
+```
+
+プログラムによって作成されたメモリマップのCSVファイルの中身を確認
+```
+$ cat mnt/memmap
+```
+
+ディスクイメージをアンマウント
+```
+$ sudo umount mnt
+```
--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,11 +1,154 @@
 #include  <Uefi.h>
 #include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Protocol/DiskIo2.h>
+#include  <Protocol/BlockIo.h>
+
+// #@@range_begin(struct_memory_map)
+// 'EFI_BOOT_SERVICES.GetMemoryMap()' から取得する情報を格納する
+struct MemoryMap {
+  UINTN buffer_size;
+  VOID* buffer;
+  UINTN map_size;
+  UINTN map_key;
+  UINTN descriptor_size;
+  UINT32 descriptor_version;
+};
+// #@@range_end(struct_memory_map)
+
+// #@@range_begin(get_memory_map)
+// 'EFI_BOOT_SERVICES.GetMemoryMap()' へのラッパ関数
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+  if (map->buffer == NULL) {
+    return EFI_BUFFER_TOO_SMALL;
+  }
+
+  map->map_size = map->buffer_size;
+  return gBS->GetMemoryMap(
+      &map->map_size,                       // IN:割り当てたバッファーサイズ、OUT:ファームウェアが割当たバッファーサイズ
+      (EFI_MEMORY_DESCRIPTOR*)map->buffer,  // OUT:ファームウェアにおける現在のメモリマップへのポインタ
+      &map->map_key,                        // OUT:現在のメモリマップを識別するための MapKey
+      &map->descriptor_size,                // OUT:EFI_MEMORY_DESCRIPTOR のサイズ
+      &map->descriptor_version);            // OUT:EFI_MEMORY_DESCRIPTOR のバージョン
+}
+// #@@range_end(get_memory_map)
+
+// #@@range_begin(get_memory_type)
+const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
+  switch (type) {
+    case EfiReservedMemoryType: return L"EfiReservedMemoryType";
+    case EfiLoaderCode: return L"EfiLoaderCode";
+    case EfiLoaderData: return L"EfiLoaderData";
+    case EfiBootServicesCode: return L"EfiBootServicesCode";
+    case EfiBootServicesData: return L"EfiBootServicesData";
+    case EfiRuntimeServicesCode: return L"EfiRuntimeServicesCode";
+    case EfiRuntimeServicesData: return L"EfiRuntimeServicesData";
+    case EfiConventionalMemory: return L"EfiConventionalMemory";
+    case EfiUnusableMemory: return L"EfiUnusableMemory";
+    case EfiACPIReclaimMemory: return L"EfiACPIReclaimMemory";
+    case EfiACPIMemoryNVS: return L"EfiACPIMemoryNVS";
+    case EfiMemoryMappedIO: return L"EfiMemoryMappedIO";
+    case EfiMemoryMappedIOPortSpace: return L"EfiMemoryMappedIOPortSpace";
+    case EfiPalCode: return L"EfiPalCode";
+    case EfiPersistentMemory: return L"EfiPersistentMemory";
+    case EfiMaxMemoryType: return L"EfiMaxMemoryType";
+    default: return L"InvalidMemoryType";
+  }
+}
+// #@@range_end(get_memory_type)
+
+// #@@range_begin(save_memory_map)
+// メモリマップをCSVファイルに変換して保存する
+EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
+  CHAR8 buf[256];
+  UINTN len;
+
+  // テーブルヘッダの書き込み
+  CHAR8* header =
+    "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+  len = AsciiStrLen(header);
+  file->Write(file, &len, header);
+
+  Print(L"map->buffer = %08lx, map->map_size = %08lx\n",
+      map->buffer, map->map_size);
+
+  // EFI_MEMORY_DESCRIPTOR パラメータを列挙した文字列を書き込む
+  EFI_PHYSICAL_ADDRESS iter;  // EFI_MEMORY_DESCRIPTORのアドレス
+  int i;                      // メモリマップの行番号<Index>
+  for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0;
+       iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
+       iter += map->descriptor_size, i++) {
+    EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter;
+    len = AsciiSPrint(
+        buf, sizeof(buf),
+        "%u, %x, %-ls, %08lx, %lx, %lx\n",
+        i, desc->Type, GetMemoryTypeUnicode(desc->Type),
+        desc->PhysicalStart, desc->NumberOfPages,
+        desc->Attribute & 0xffffflu);
+    file->Write(file, &len, buf);
+  }
+
+  return EFI_SUCCESS;
+}
+// #@@range_end(save_memory_map)
+
+EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
+  EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
+
+  gBS->OpenProtocol(
+      image_handle,
+      &gEfiLoadedImageProtocolGuid,
+      (VOID**)&loaded_image,
+      image_handle,
+      NULL,
+      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  gBS->OpenProtocol(
+      loaded_image->DeviceHandle,
+      &gEfiSimpleFileSystemProtocolGuid,
+      (VOID**)&fs,
+      image_handle,
+      NULL,
+      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  fs->OpenVolume(fs, root);
+
+  return EFI_SUCCESS;
+}
 
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE *system_table) {
+  // 起動メッセージ
   Print(L"Hello, Mikan World!\n");
   Print(L"built by raisuta.\n");
+
+  // 配列によるバッファを確保して、メモリマップを読み出す - EFI_BOOT_SERVICES.GetMemoryMap()
+  CHAR8 memmap_buf[4096 * 4];
+  struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
+  GetMemoryMap(&memmap);
+
+  // RootDirへのファイルハンドルを取得 - EFI_BOOT_SERVICES.OpenProtocol()
+  EFI_FILE_PROTOCOL* root_dir;
+  OpenRootDir(image_handle, &root_dir);
+
+  // ファイルを開く - EFI_FILE_PROTOCOL.Open() 
+  EFI_FILE_PROTOCOL* memmap_file;
+  root_dir->Open(
+      root_dir, &memmap_file, L"\\memmap",
+      EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+
+  // ファイルへメモリマップを保存
+  SaveMemoryMap(&memmap, memmap_file);
+  memmap_file->Close(memmap_file);
+  // #@@range_end(main)
+
+  Print(L"All done\n");
+
   while (1);
   return EFI_SUCCESS;
 }
